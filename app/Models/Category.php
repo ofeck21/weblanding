93fe9a6ea5375a1d9aef5cdcae7c4de6d993ca59
rofik <ofecktux@gmail.com>
2024-03-15 23:27:35 +0700

    update category table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    // boot method
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        // generate slug before creating a new category
+        static::creating(function ($category) {
+            //generate unique slug by appending a number to the slug
+            $originalSlug = $slug = \Str::slug($category->name);
+            $count = 1;
+            while (static::whereSlug($slug)->exists()) {
+                $slug = $originalSlug . '-' . $count++;
+            }
+            $category->slug = $slug;
+        });
+    }
+
+    public function templates(): object
+    {
+        return $this->hasMany(Template::class);
+    }
+
 }
