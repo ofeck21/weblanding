acfab57bb787aa8b13cfb516eb7e4b6b85dec7c3
rofik <ofecktux@gmail.com>
2024-03-18 11:48:21 +0700

    update faq page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faq;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function faq() {
-        return view('landing-page.page.faq');
+        $data['faqs'] = Faq::all();
+        return view('landing-page.page.faq', $data);
     }
 }
--- resources/views/landing-page/page/faq.blade.php
+++ resources/views/landing-page/page/faq.blade.php
@@ -9,7 +9,7 @@
             <div class="page-title-row">
                 <div class="page-title-wrapper">
                     <div class="page-title-box">
-                        <h1 class="page-title">Frequently <em>Asked</em><br> Questions</h1>
+                        <h1 class="page-title">Pertanyaan <em>Yang Sering</em><br> Ditanyakan</h1>
                     </div>
                 </div>
             </div>
@@ -25,148 +25,44 @@
                     <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-f6ced3b"
                         data-id="f6ced3b" data-element_type="column">
                         <div class="elementor-widget-wrap elementor-element-populated">
-                            <div class="elementor-element elementor-element-11182f9 elementor-widget elementor-widget-text-editor"
-                                data-id="11182f9" data-element_type="widget" data-widget_type="text-editor.default">
-                                <div class="elementor-widget-container">
-                                    <h5>1. How to work with Google Analytics?</h5>
-                                    <p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas. Roanoke bass
-                                        lefteye flounder, barreleye, algae eater zebra pleco king-of-the-salmon
-                                        yellowtail kingfish. Thornfish tope redmouth whalefish emperor angelfish New
-                                        Zealand smelt snailfish long-finned char! Ballan wrasse, pikeblenny, turbot
-                                        featherback dwarf gourami wallago European perch barfish monkfish snake</p>
+                            @foreach ($faqs as $key => $faq)
+                                @if($key < 4)
+                                <div class="elementor-element elementor-element-{{$faq->id}} elementor-widget elementor-widget-text-editor" data-id="{{$faq->id }}" data-element_type="widget" data-widget_type="text-editor.default">
+                                    <div class="elementor-widget-container">
+                                        <h5>{{$key+1}}. {!! $faq->question !!}</h5>
+                                        <p>{!! $faq->answer !!}</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="elementor-element elementor-element-e0bcf68 elementor-widget elementor-widget-text-editor"
-                                data-id="e0bcf68" data-element_type="widget" data-widget_type="text-editor.default">
-                                <div class="elementor-widget-container">
-                                    <h5>2. Where I can find marketing solutions for my Instagram?</h5>
-                                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-                                        sed quia consequuntur! Ballan wrasse, pikeblenny, turbot featherback dwarf
-                                        gourami wallago</p>
-                                </div>
-                            </div>
-                            <div class="elementor-element elementor-element-4b33383 elementor-widget elementor-widget-text-editor"
-                                data-id="4b33383" data-element_type="widget" data-widget_type="text-editor.default">
-                                <div class="elementor-widget-container">
-                                    <h5>3. How to work with Marketum dashboard?</h5>
-                                    <p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas. Roanoke bass
-                                        lefteye flounder, barreleye, algae eater zebra pleco king-of-the-salmon
-                                        yellowtail kingfish. Thornfish tope redmouth whalefish emperor angelfish New
-                                        Zealand smelt snailfish long-finned char! Ballan wrasse, pikeblenny, turbot
-                                        featherback dwarf gourami wallago European perch barfish monkfish snake</p>
-                                </div>
-                            </div>
-                            <div class="elementor-element elementor-element-27889f7 elementor-widget elementor-widget-text-editor"
-                                data-id="27889f7" data-element_type="widget" data-widget_type="text-editor.default">
-                                <div class="elementor-widget-container">
-                                    <h5>4. Can I change analytics admin panel?</h5>
-                                    <p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas. Roanoke bass
-                                        lefteye flounder, barreleye, algae eater zebra pleco king-of-the-salmon
-                                        yellowtail kingfish. Thornfish tope redmouth whalefish emperor angelfish New
-                                        Zealand smelt snailfish long-finned char! Ballan wrasse, pikeblenny, turbot
-                                        featherback dwarf gourami wallago European perch barfish monkfish snake</p>
-                                </div>
-                            </div>
+                                @endif
+                            @endforeach
                             <div class="elementor-element elementor-element-8ab127e elementor-widget elementor-widget-toggle"
                                 data-id="8ab127e" data-element_type="widget" data-widget_type="toggle.default">
                                 <div class="elementor-widget-container">
                                     <div class="elementor-toggle">
-                                        <div class="elementor-toggle-item">
-                                            <div id="elementor-tab-title-1451" class="elementor-tab-title"
-                                                data-tab="1" role="button"
-                                                aria-controls="elementor-tab-content-1451" aria-expanded="false">
-                                                <span class="elementor-toggle-icon elementor-toggle-icon-right"
-                                                    aria-hidden="true">
-                                                    <span class="elementor-toggle-icon-closed"><i
-                                                            class="fontello icon-plus"></i></span>
-                                                    <span class="elementor-toggle-icon-opened"><i
-                                                            class="elementor-toggle-icon-opened fontello icon-minus"></i></span>
-                                                </span>
-                                                <a class="elementor-toggle-title" tabindex="0">How to work with
-                                                    Google Analytics?</a>
-                                            </div>
+                                        @foreach ($faqs as $key => $faq)
+                                            @if($key > 4)
+                                            <div class="elementor-toggle-item">
+                                                <div id="elementor-tab-title-{{$faq->id}}" class="elementor-tab-title"
+                                                    data-tab="{{$faq->id}}" role="button"
+                                                    aria-controls="elementor-tab-content-{{$faq->id}}" aria-expanded="false">
+                                                    <span class="elementor-toggle-icon elementor-toggle-icon-right"
+                                                        aria-hidden="true">
+                                                        <span class="elementor-toggle-icon-closed"><i
+                                                                class="fontello icon-plus"></i></span>
+                                                        <span class="elementor-toggle-icon-opened"><i
+                                                                class="elementor-toggle-icon-opened fontello icon-minus"></i></span>
+                                                    </span>
+                                                    <a class="elementor-toggle-title" tabindex="0">{!! $faq->question !!}</a>
+                                                </div>
 
-                                            <div id="elementor-tab-content-1451"
-                                                class="elementor-tab-content elementor-clearfix" data-tab="1"
-                                                role="region" aria-labelledby="elementor-tab-title-1451">
-                                                <p>Reedfish bonefish trahira bristlenose catfish, longnose
-                                                    lancetfish morid. Wahoo mora deep sea smelt cat shark atlantic.
-                                                    Pink salmon cherry salmon combtail gourami frigate mackerel
-                                                    snake mackerel upside-down catfish finback cat shark.</p>
+                                                <div id="elementor-tab-content-{{$faq->id}}"
+                                                    class="elementor-tab-content elementor-clearfix" data-tab="{{$faq->id}}"
+                                                    role="region" aria-labelledby="elementor-tab-title-{{$faq->id}}">
+                                                    <p>{!! $faq->answer !!}</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="elementor-toggle-item">
-                                            <div id="elementor-tab-title-1452" class="elementor-tab-title"
-                                                data-tab="2" role="button"
-                                                aria-controls="elementor-tab-content-1452" aria-expanded="false">
-                                                <span class="elementor-toggle-icon elementor-toggle-icon-right"
-                                                    aria-hidden="true">
-                                                    <span class="elementor-toggle-icon-closed"><i
-                                                            class="fontello icon-plus"></i></span>
-                                                    <span class="elementor-toggle-icon-opened"><i
-                                                            class="elementor-toggle-icon-opened fontello icon-minus"></i></span>
-                                                </span>
-                                                <a class="elementor-toggle-title" tabindex="0">Where I can find
-                                                    marketing solutions for my Instagram?</a>
-                                            </div>
-
-                                            <div id="elementor-tab-content-1452"
-                                                class="elementor-tab-content elementor-clearfix" data-tab="2"
-                                                role="region" aria-labelledby="elementor-tab-title-1452">
-                                                <p>Reedfish bonefish trahira bristlenose catfish, longnose
-                                                    lancetfish morid. Wahoo mora deep sea smelt cat shark atlantic.
-                                                    Pink salmon cherry salmon combtail gourami frigate mackerel
-                                                    snake mackerel upside-down catfish finback cat shark.</p>
-                                            </div>
-                                        </div>
-                                        <div class="elementor-toggle-item">
-                                            <div id="elementor-tab-title-1453" class="elementor-tab-title"
-                                                data-tab="3" role="button"
-                                                aria-controls="elementor-tab-content-1453" aria-expanded="false">
-                                                <span class="elementor-toggle-icon elementor-toggle-icon-right"
-                                                    aria-hidden="true">
-                                                    <span class="elementor-toggle-icon-closed"><i
-                                                            class="fontello icon-plus"></i></span>
-                                                    <span class="elementor-toggle-icon-opened"><i
-                                                            class="elementor-toggle-icon-opened fontello icon-minus"></i></span>
-                                                </span>
-                                                <a class="elementor-toggle-title" tabindex="0">How to work with
-                                                    Marketum dashboard?</a>
-                                            </div>
-
-                                            <div id="elementor-tab-content-1453"
-                                                class="elementor-tab-content elementor-clearfix" data-tab="3"
-                                                role="region" aria-labelledby="elementor-tab-title-1453">
-                                                <p>Reedfish bonefish trahira bristlenose catfish, longnose
-                                                    lancetfish morid. Wahoo mora deep sea smelt cat shark atlantic.
-                                                    Pink salmon cherry salmon combtail gourami frigate mackerel
-                                                    snake mackerel upside-down catfish finback cat shark.</p>
-                                            </div>
-                                        </div>
-                                        <div class="elementor-toggle-item">
-                                            <div id="elementor-tab-title-1454" class="elementor-tab-title"
-                                                data-tab="4" role="button"
-                                                aria-controls="elementor-tab-content-1454" aria-expanded="false">
-                                                <span class="elementor-toggle-icon elementor-toggle-icon-right"
-                                                    aria-hidden="true">
-                                                    <span class="elementor-toggle-icon-closed"><i
-                                                            class="fontello icon-plus"></i></span>
-                                                    <span class="elementor-toggle-icon-opened"><i
-                                                            class="elementor-toggle-icon-opened fontello icon-minus"></i></span>
-                                                </span>
-                                                <a class="elementor-toggle-title" tabindex="0">Can I change
-                                                    analytics admin panel?</a>
-                                            </div>
-
-                                            <div id="elementor-tab-content-1454"
-                                                class="elementor-tab-content elementor-clearfix" data-tab="4"
-                                                role="region" aria-labelledby="elementor-tab-title-1454">
-                                                <p>Reedfish bonefish trahira bristlenose catfish, longnose
-                                                    lancetfish morid. Wahoo mora deep sea smelt cat shark atlantic.
-                                                    Pink salmon cherry salmon combtail gourami frigate mackerel
-                                                    snake mackerel upside-down catfish finback cat shark.</p>
-                                            </div>
-                                        </div>
+                                            @endif
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
